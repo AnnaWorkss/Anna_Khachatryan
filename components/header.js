e70874af11fb418893f5fcd19a83a72fb2eaf9a4
Anna Khachatryan <annakhachatryan@Annas-MacBook-Pro.local>
2026-03-28 00:51:22 +0400

    minor changes

---
+++ components/header.js
@@ -0,0 +1,35 @@
+class CommonHeader extends HTMLElement {
+    connectedCallback() {
+        const activePage = this.getAttribute('active-page') || 'home';
+        
+        this.innerHTML = `
+            <header class="header">
+                <nav class="navbar">
+                    <ul class="navbar">
+                        <li class="navbar_section_name ${activePage === 'home' ? 'active_page' : ''}">
+                            <a href="../home/index.html">Home</a>
+                        </li>
+                        <li class="navbar_section_devider"></li>
+                        <li class="navbar_section_name ${activePage === 'catalog' ? 'active_page' : ''}">
+                            <a href="../catalog/index.html">Catalog</a>
+                        </li>
+                        <li class="navbar_section_devider"></li>
+                        <li class="navbar_section_name ${activePage === 'about' ? 'active_page' : ''}">
+                            <a href="../about/index.html">About</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="logo">
+                    <img class="logo_img" src="../../assets/images/Logo.png" alt="logo">
+                </div>
+                <div class="header_info">
+                    <span class="header_info_text">
+                        I’m Anna, freelancer from Armenia, developing through real-world experience and hands-on work with a swiss advertising agency. Driven by curiosity and rapid growth.
+                    </span>
+                </div>
+            </header>
+        `;
+    }
+}
+
+customElements.define('common-header', CommonHeader);
